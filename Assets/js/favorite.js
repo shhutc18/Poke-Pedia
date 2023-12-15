be1746ef45632bfec72f050db3465dea43c02199
jrsevi <jrsevi@gmail.com>
2023-12-14 20:26:40 -0800

    Saves as an array

--- Assets/js/favorite.js
+++ Assets/js/favorite.js
@@ -7,4 +7,18 @@ let pokemonName = document.getElementById('pokemonNameEl');
 favoriteButton.addEventListener('click', function() {
     localStorage.setItem('pokemonSprite', pokemonSprite.innerHTML);
     localStorage.setItem('pokemonName', pokemonName.innerHTML);
+
+    let newFavorite= {
+        sprite: pokemonSprite.innerHTML,
+        name: pokemonName.innerHTML
+    };
+    let favorites = JSON.parse(localStorage.getItem('favorites'));
+
+    if (!favorites) {
+        favorites = [];
+    }
+
+    favorites.push(newFavorite);
+
+    localStorage.setItem('favorites', JSON.stringify(favorites));
 });
